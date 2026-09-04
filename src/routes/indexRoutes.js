ae5ca892e877a45ff2603e27dddc01f4ba4e1eb9
Joshua <jaboytes.sebastian@gmail.com>
2026-09-04 10:33:16 -0600

    Version2Sal

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -9,8 +9,37 @@ router.get('/', async(req,res) => {
     res.render("index", {productos: productos});
 });
 
-router.get('/update', (req,res) => {
-    res.render("editar");
+router.get('/update/:id', async (req,res) => {
+    try{
+        const productos = await Productos.findById(req.params.id).lean();
+        res.render("editar", {productos});
+    }catch(error){
+        console.log(error);
+
+    }
+
+});
+
+router.post('/update/:id',async(req,res) =>{
+    try{
+        const {id} = req.params;
+        await Productos.findByIdAndUpdate(id, req.body);
+        res.redirect("/");
+    }catch(error){
+        console.log(error);
+        res.redirect("/");
+    }
+});
+
+router.post('/delete/:id', async (req,res) => {
+    try{
+        const {id} = req.params;
+        await Productos.findByIdAndDelete(id);
+        res.redirect("/");
+    }catch(error){
+        console.log(error);
+        res.redirect("/");
+    }
 });
 
 router.post('/productos/agregar', async (req,res) => {
