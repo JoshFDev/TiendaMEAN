db08d8b4571877feab198218f9ce49061c06bf96
Joshua <jaboytes.sebastian@gmail.com>
2026-09-12 15:32:37 -0600

    Configura conexion con MongoDB Atlas

--- src/database.js
+++ src/database.js
@@ -1,12 +1,21 @@
-import {connect } from "mongoose";
+import { connect } from "mongoose";
+
 import { MONGODB_URI } from "./config";
 
+console.log(
+    "MONGODB_URI:",
+    MONGODB_URI?.replace(/:([^:@]+)@/, ":****@")
+);
+
 (async () => {
-    try{
+    try {
         const db = await connect(MONGODB_URI);
-        console.log("Database conected to", db.connection.name);
-    }catch(error){
+
+        console.log("Database connected to:", db.connection.name);
+        console.log("MongoDB readyState:", db.connection.readyState);
+
+    } catch (error) {
+        console.log("ERROR DE MONGODB:");
         console.log(error);
     }
 })();
-
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import app from './app';
+
 import './database';
+
 import { PORT } from './config';
 
+app.listen(PORT);
 
-app.listen(3000);
 console.log('Server on port', PORT);
